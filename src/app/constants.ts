c58b38dce8dc6dfe0733c0aa0e46cc1515488cb6
maqsusi <akshu.manutd@gmail.com>
2021-12-21 18:08:19 +0530

    changed http to https

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,5 +1,5 @@
 export class Constants {
   public static API_KEY = 'cfacfc07a93e7dc76a08f8722aca95aa';
-  public static API_URL = 'http://api.openweathermap.org/data/2.5/';
-  public static ICON_URL = 'http://openweathermap.org/img/wn/';
+  public static API_URL = 'https://api.openweathermap.org/data/2.5/';
+  public static ICON_URL = 'https://openweathermap.org/img/wn/';
 }
